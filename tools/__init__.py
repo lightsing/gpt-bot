3f5f60f66ab929aa071a001b344c50d2cff217c6
lightsing <light.tsing@gmail.com>
2023-05-09 18:54:00 +0800

    using json io

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,4 +1,4 @@
-import logging
+import json
 
 import openai
 from aiocache import cached, Cache
@@ -10,36 +10,51 @@ from .nonsense import gen_nonsense
 
 logger = logging.getLogger("bot")
 
-
-@cached(cache=Cache.REDIS, serializer=JsonSerializer())
+@cached(
+    cache=Cache.REDIS,
+    ttl=3600,
+    serializer=JsonSerializer(),
+    namespace="route",
+    skip_cache_func=lambda r: r is None
+)
 async def route(req: str):
     prompt = """根据用户的输入，判断要调用哪个函数。你可以调用的函数有：
-1. 今日运势 gen_yunshi
-2. 今日食物 gen_food
-4. 除外的任何其他情况下都输出 gen_nonsense
-请返回函数名，不要输出其他东西。
+1. 日常问候 gen_greeting
+2. 今日运势 gen_yunshi
+3. 今日食物 gen_food
+4. 给用户起名 gen_naming
+5. 除外的任何其他情况下都输出 gen_nonsense
 例如：
-用户：今晚吃什么；输出：gen_food
-用户：嘿！；输出：gen_nonsense
-用户：张三可以吃吗；输出：gen_nonsense
+{"user_msg": "早上好"} 输出：{"action": "gen_greeting"}
+{"user_msg": "晚安"} 输出：{"action": "gen_greeting"}
+{"user_msg": "今晚吃什么"} 输出：{"action": "gen_food"}
+{"user_msg": "你打算叫我什么"} 输出：{"action": "gen_naming"}
+{"user_msg": "帮我起个昵称"} 输出：{"action": "gen_naming"}
+{"user_msg": "嘿！"} 输出：{"action": "gen_nonsense"}
+{"user_msg": "张三可以吃吗"} 输出：{"action": "gen_nonsense"}
 """
     temperature = 0.1
     max_tokens = 10
 
-    completion = await openai.ChatCompletion.acreate(
-        model="gpt-3.5-turbo",
-        temperature=temperature,
-        messages=[
-            {"role": "system", "content": prompt},
-            {"role": "user", "content": req}
-        ],
-        max_tokens=max_tokens,
-        n=1,
-    )
-    choice = completion['choices'][0]['message']['content']
-    choice = choice.encode("ascii", errors="ignore").decode()
+    for _ in range(10):
+        completion = await openai.ChatCompletion.acreate(
+            model="gpt-3.5-turbo",
+            temperature=temperature,
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": req}
+            ],
+            max_tokens=max_tokens,
+            n=1,
+        )
+        choice = completion['choices'][0]['message']['content']
+        logger.info(f'cache miss for {req}, raw choice: {choice}')
+        try:
+            return json.loads(choice)["action"]
+        except Exception as e:
+            logger.warn(f"error occur when processing {e}")
 
-    return choice
+    return None
 
 
 async def route_mapping(req: str):
